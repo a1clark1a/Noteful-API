5782b294574a422cb5ab80c51fbc9f8e4ce5c51c
Anthony Clark Perfecto <a1clark1a@gmail.com>
2020-01-29 21:14:46 -0800

    Initial Setup Commit

---
+++ src/app.js
@@ -0,0 +1,27 @@
+require("dotenv").config();
+const express = require("express");
+const morgan = require("morgan");
+const cors = require("cors");
+const helmet = require("helmet");
+const { NODE_ENV } = require("./config");
+const errorHandler = require("./errorHandler");
+
+const app = express();
+
+const morganOption = NODE_ENV === "production" ? " tiny" : "common";
+
+app.use(morgan(morganOption));
+app.use(helmet());
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.send("Hello, world!");
+});
+
+//TODO ADD API VALIDATOR
+
+//TODO USE ROUTERS FOR FOLDER AND NOTES
+
+app.use(errorHandler);
+
+module.exports = app;
