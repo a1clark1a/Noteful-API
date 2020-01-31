1491f97b650018ec28f60a37b796993a55c168b1
Anthony Clark Perfecto <a1clark1a@gmail.com>
2020-01-31 12:18:52 -0800

    added seeds for database

---
+++ seeds/seed.notes.sql
@@ -0,0 +1,12 @@
+INSERT INTO noteful_notes(name, modified, content, folders_id )
+VALUES
+('Notes 1', '2029-01-22T16:28:32.615Z', 'Some notes here...', 1),
+('Notes 2', '2029-01-22T16:28:32.615Z', 'Some notes here...', 2),
+('Notes 3', '2021-01-22T16:28:32.615Z', 'Some notes here...', 1),
+('Notes 4', '2016-03-22T16:28:32.615Z', 'Some notes here...', 1),
+('Notes 5', '2029-01-22T16:28:32.615Z', 'Some notes here...', 3),
+('Notes 6', '2029-05-22T16:28:32.615Z', 'Some notes here...', 4),
+('Notes 7', '2029-01-22T16:28:32.615Z', 'Some notes here...', 5),
+('Notes 8', '2001-06-22T16:28:32.615Z', 'Some notes here...', 2),
+('Notes 9', '2000-01-22T16:28:32.615Z', 'Some notes here...', 3),
+('Notes 10', '1989-01-22T16:28:32.615Z','Some notes here...', 1);
